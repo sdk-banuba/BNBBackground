87ea314c49cc9a1b491450358ee6fb9e801336f4
ci <ci@banuba.com>
2023-11-24 17:07:30 +0300

    1.9.1-86-g65c1c833d4

---
+++ Package.swift
@@ -0,0 +1,70 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.9.1-86-g65c1c833d4"
+
+let package = Package(
+    name: "BNBBackground",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBBackground",
+            targets: [
+                "BNBBackground",
+                "BNBBackground_BNBSdkCore",
+                "BNBBackground_BNBEffectPlayer",
+                "BNBBackground_BNBScripting"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBEffectPlayer.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBScripting.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBBackground",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-86-g65c1c833d4/BNBBackground.zip",
+            checksum: "eacf80c5c513e6b46860483544f57b03a69114c2028aa8bb16c8bf5c59db6a44"
+        ),
+        .target(
+            name: "BNBBackground_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBBackground_BNBEffectPlayer",
+            dependencies: [
+                .product(
+                    name: "BNBEffectPlayer",
+                    package: "BNBEffectPlayer"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBBackground_BNBScripting",
+            dependencies: [
+                .product(
+                    name: "BNBScripting",
+                    package: "BNBScripting"
+                ),
+            ]
+        ),
+    ]
+)
